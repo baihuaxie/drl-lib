65861f2aed074bd627d9907e164889b639f3756a
Baihua Xie <xie_baihua@163.com>
2020-10-08 21:13:26 +0800

    10/08: add normalize_observation() method to PolicyWithValue class

--- baselines/common/policy_util.py
+++ baselines/common/policy_util.py
@@ -22,6 +22,12 @@ class PolicyWithValue(object):
 
         raise NotImplementedError
 
+    def normalize_observations(self, observations):
+        """
+        Normalizes the observation space
+        """
+        raise NotImplementedError
+
     def step(self, observation, **extra_feed):
         """
         Compute the next action(s) given the observation(s)
@@ -42,13 +48,14 @@ class PolicyWithValue(object):
         raise NotImplementedError
 
 
-def build_policy(env, policy_network, value_network=None, **policy_kwargs):
+def build_policy(env, policy_network, value_network=None, normalize_observations=False, **policy_kwargs):
     """
     Policy / Value network builder
 
     Args:
         env: (gym.Env class) environment
         policy_network: (str) name for the policy network type
+        normalize_observations: (bool) if true normalizes and clips the observation space
         **policy_kwargs: pointer to arguments to instantiate the policy network
 
     Returns:
@@ -62,4 +69,13 @@ def build_policy(env, policy_network, value_network=None, **policy_kwargs):
         # Q: is this redundant? because get_network_builder already has a check for
         # if 'policy_network' name is callable
         network_type = policy_network
+        # policy_network is a customized (by network_type & **policy_kwargs) network object
         policy_network = get_network_builder(network_type)(**policy_kwargs)
+
+        def policy_fn(nbatch=None, nsteps=None):
+            """
+            A function to return a policy network + methods
+            """
+            # get observation space
+            ob_space = env.observation_space
+
